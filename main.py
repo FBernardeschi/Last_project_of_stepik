b8f632c816f8cc3878ddb6a259844b1b9b8815f5
FBernardeschi <kkostik100@yandex.ru>
2022-04-01 21:06:12 +0500

    Переделал в Page Object

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-import pytest
-import time
+from .main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    browser.implicitly_wait(15)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    time.sleep(10)
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
